d8513797cbbfc7e5d17cf98d9994ae2a7751b38b
DJGraham99 <dylanjosephgraham@gmail.com>
2022-10-28 16:35:08 +0100

    Added static counter to Treasure class + format Entity.

--- src/main/java/com/sprint/game_text/Entity.java
+++ src/main/java/com/sprint/game_text/Entity.java
@@ -4,29 +4,25 @@ public class Entity {
 
 	protected int[] position;
 	private char sprite;
-	
-	public Entity(int [] position) {
+
+	public Entity(int[] position) {
 		this.position = position;
 	}
-	
+
 	public int[] getPosition() {
 		return this.position;
 	}
-	
+
 	public void setPosition(int[] new_position) {
 		this.position = new_position;
 	}
 
-
 	public char getSprite() {
 		return sprite;
 	}
-	
-	
 
 	public void setSprite(char sprite) {
 		this.sprite = sprite;
 	}
-	
-	
+
 }
--- src/main/java/com/sprint/game_text/Treasure.java
+++ src/main/java/com/sprint/game_text/Treasure.java
@@ -1,13 +1,13 @@
 package com.sprint.game_text;
 
 public class Treasure extends Entity {
-	
+
+	protected static int instanceCounter = 0;
+
 	public Treasure(int[] position) {
 		super(position);
 		this.setSprite('T');
+		instanceCounter++;
 	}
-	
-	
-	
 
 }
