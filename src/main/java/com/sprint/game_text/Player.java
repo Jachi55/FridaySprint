eaf120e3d2e323071f365b27e6a4dacc32ec568d
AjjerTheBadger <ajmalbari@outlook.com>
2022-10-28 14:13:18 +0100

    Change position to int[] and add helper methods

--- src/main/java/com/sprint/game_text/Player.java
+++ src/main/java/com/sprint/game_text/Player.java
@@ -3,19 +3,43 @@ package com.sprint.game_text;
 import java.util.Scanner;
 
 public class Player {
-	private int x;
-	private int y;
+	private int[] position;
 	private String name;
-	 Scanner input = new Scanner(System.in);
-	 
-	 
-	 public Player (String name, int x, int y) {
-		 
-		 this.name = name;
-		 this.x = x;
-		 this.y = y;
-		 
-	 }
+	private char sprite;
+	Scanner input = new Scanner(System.in);
 	
+	public Player (String name, int x, int y) {
+		 this.setName(name);
+		 this.position[0] = x;
+		 this.position[1] = y;
+		 this.setSprite('P');
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int[] getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition(int[] new_position) {
+		this.position = new_position;
+	}
+
+
+	public char getSprite() {
+		return sprite;
+	}
+
+
+	public void setSprite(char sprite) {
+		this.sprite = sprite;
+	}
 
 }
