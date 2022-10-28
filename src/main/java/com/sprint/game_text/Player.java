d8686df4eb55ad97602dee175cef1a565e29554a
Jachi <acejachi@gmail.com>
2022-10-28 13:57:53 +0100

    Player Class

---
+++ src/main/java/com/sprint/game_text/Player.java
@@ -0,0 +1,21 @@
+package com.sprint.game_text;
+
+import java.util.Scanner;
+
+public class Player {
+	private int x;
+	private int y;
+	private String name;
+	 Scanner input = new Scanner(System.in);
+	 
+	 
+	 public Player (String name, int x, int y) {
+		 
+		 this.name = name;
+		 this.x = x;
+		 this.y = y;
+		 
+	 }
+	
+
+}
