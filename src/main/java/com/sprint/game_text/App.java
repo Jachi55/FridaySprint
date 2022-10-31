43d33589096e869e9033b84ef3ef478d10c15e64
Jachi <acejachi@gmail.com>
2022-10-31 11:45:38 +0000

    multiple treasure

--- src/main/java/com/sprint/game_text/App.java
+++ src/main/java/com/sprint/game_text/App.java
@@ -1,106 +1,137 @@
 package com.sprint.game_text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
-	
-  public static void main(String[] args) {
-	  
-	//======================== GAME SETUP ========================
-    Scanner scanner = new Scanner (System.in);
-    char move;
-    int distanceFromTreasure;
 
-    
-    System.out.println("Welcome to Gridlock!!");
-    System.out.println("You can make this grid as big as you like");
-    System.out.println("How many rows would you like?");
-    int rows = scanner.nextInt();
+	public static void main(String[] args) {
 
-    System.out.println("How many columns would you like?");
-    int columns = scanner.nextInt(); 
-    Grid gameGrid = new Grid(rows,columns);
-    
-    System.out.println("What is your name?");
-    String name = scanner.next() ;
-    
-    
-    int [] playerLocation = generateCoordinates(rows, columns);
-    Player player = new Player(name, playerLocation);
-    int treasureLocation[] = generateCoordinates(rows,columns);
-    while (Arrays.equals(playerLocation,treasureLocation)) {
-    	treasureLocation = generateCoordinates(rows,columns);
-    }
-    
-    Treasure treasure = new Treasure (treasureLocation);
-    
-    gameGrid.updateTile(treasure.getPosition(), treasure);
-    gameGrid.updateTile(player.getPosition(), player);
-    
-    gameGrid.show();
-    
-    int [] pos;
-    
-    // GAME LOOP 
-    
-    while (true) {
+		// ======================== GAME SETUP ========================
+		Scanner scanner = new Scanner(System.in);
+		char move;
+		int noOfTreasure;
+		ArrayList<Treasure> treasure = new ArrayList<Treasure>();
+		int[] treasureLocation;
+		boolean win = false;
 
-    	if (Arrays.equals(player.getPosition(), treasure.getPosition())) {
+		System.out.println("Welcome to Gridlock!!");
+		System.out.println("You can make this grid as big as you like");
+		System.out.println("How many rows would you like?");
+		int rows = scanner.nextInt();
 
-    		
-    		System.out.println("\n Congratulations you have found the treasure! YOU WIN!");
-    		break;
-    		
-    	}
-    	distanceFromTreasure = getDistance(player,treasure);
-    	System.out.println("You are " + distanceFromTreasure + "m away from the Treasure" );
-    	System.out.println("\n Use 'wsad' to move!");
-    	move = scanner.next().charAt(0);
-    	pos = player.getPosition();
-    	
-    	// Boundary Control 
-    	
-    	if ((pos[1] == columns-1  && move == 'd') || pos[1] <= 0 && move == 'a' || pos[0] == rows-1 && move == 's' || pos[0] <= 0 && move == 'w') {
-    		
-    		System.out.println("Out of Bounds!!");
-    	}
-    	
-    	else {
-    	
-        player.movePlayer(move);
-    	gameGrid.updateTile(player.getPosition(), player);
-    	gameGrid.show();
+		System.out.println("How many columns would you like?");
+		int columns = scanner.nextInt();
+		Grid gameGrid = new Grid(rows, columns);
 
-    	
-    }
-    }
-    
-    
-    scanner.close();
-    
-  }
-  
-  public static int [] generateCoordinates(int max_x, int max_y) {
-	  double x = Math.random();
-	  double y = Math.random();
-	  x = x * max_x;
-	  y = y * max_y;
-	  
-	  int [] coords = {(int) x, (int) y};
-	  
-	  return coords;
-  }
-  
-  // Calculates Distance
-  public static int getDistance(Entity e1, Entity e2) {
-	  
-	  int[] e1Position = e1.getPosition();
-	  int[] e2Position = e2.getPosition();
-	  
-	  double[] distanceVector = {e1Position[0]-e2Position[0], e1Position[1]-e2Position[1]};
-	  double absDistance = Math.sqrt( ( distanceVector[0] * distanceVector[0] ) + ( distanceVector[1] * distanceVector[1] ) );
-	  
-	  return (int) absDistance;
-  }
+		System.out.println("How many pieces of treasure would you like to find?");
+		noOfTreasure = scanner.nextInt();
+
+		System.out.println("What is your name?");
+		String name = scanner.next();
+
+		int[] playerLocation = generateCoordinates(rows, columns);
+		Player player = new Player(name, playerLocation);
+
+		int i = 0;
+		while (i < noOfTreasure) {
+			treasureLocation = generateCoordinates(rows, columns);
+
+				while (Arrays.equals(playerLocation, treasureLocation)) {
+					treasureLocation = generateCoordinates(rows, columns);
+				}
+			treasure.add(new Treasure (treasureLocation));
+			gameGrid.updateTile(treasure.get(i).getPosition(), treasure.get(i));
+			i++;
+		}
+
+	gameGrid.updateTile(player.getPosition(),player);
+
+	gameGrid.show();
+
+	int[] pos;
+	int d = 100;
+
+	// GAME LOOP
+
+	while(true)
+	{
+
+		for (Treasure t : treasure) {
+
+			if (Arrays.equals(player.getPosition(), t.getPosition())) {
+
+				if (treasure.size() != 1) {
+					treasure.remove(t);
+					System.out.println("Congratulations you have found the treasure! There are " + treasure.size()
+							+ " pieces left on the map!");
+					treasure.remove(t);
+				} else {
+					System.out.println("Congratulations you have found the treasure! YOU WIN!");
+					win = true;
+					break;
+				}
+			}
+		}
+		
+
+		if (win == true) {
+			break;
+		}
+
+
+		for (Treasure t : treasure) {
+			d = Math.min(getDistance(player, t), d);
+		}
+
+		System.out.println("You are " + d + "m away from the closest Treasure");
+		System.out.println("\n Use 'wsad' to move!");
+		move = scanner.next().charAt(0);
+		pos = player.getPosition();
+
+		// Boundary Control
+
+		if ((pos[1] == columns - 1 && move == 'd') || pos[1] <= 0 && move == 'a' || pos[0] == rows - 1 && move == 's'
+				|| pos[0] <= 0 && move == 'w') {
+
+			System.out.println("Out of Bounds!!");
+		}
+
+		else {
+
+			player.movePlayer(move);
+			gameGrid.updateTile(player.getPosition(), player);
+			gameGrid.show();
+
+		}
+	}
+
+	scanner.close();
+
+	}
+
+	public static int[] generateCoordinates(int max_x, int max_y) {
+		double x = Math.random();
+		double y = Math.random();
+		x = x * max_x;
+		y = y * max_y;
+
+		int[] coords = { (int) x, (int) y };
+
+		return coords;
+	}
+
+	// Calculates Distance
+	public static int getDistance(Entity e1, Entity e2) {
+
+		int[] e1Position = e1.getPosition();
+		int[] e2Position = e2.getPosition();
+
+		double[] distanceVector = { e1Position[0] - e2Position[0], e1Position[1] - e2Position[1] };
+		double absDistance = Math
+				.sqrt((distanceVector[0] * distanceVector[0]) + (distanceVector[1] * distanceVector[1]));
+
+		return (int) absDistance;
+	}
 }
--- src/main/java/com/sprint/game_text/Treasure.java
+++ src/main/java/com/sprint/game_text/Treasure.java
@@ -2,12 +2,10 @@ package com.sprint.game_text;
 
 public class Treasure extends Entity {
 
-	protected static int instanceCounter = 0;
 
 	public Treasure(int[] position) {
 		super(position);
 		this.setSprite('T');
-		instanceCounter++;
 	}
 
 }
