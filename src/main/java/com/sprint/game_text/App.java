3019ef6ecb18eb5d6d1de59be65f95d866df940b
AjjerTheBadger <ajmalbari@outlook.com>
2022-10-31 17:11:33 +0000

    Continue Debugging randomMove()

--- src/main/java/com/sprint/game_text/App.java
+++ src/main/java/com/sprint/game_text/App.java
@@ -42,6 +42,15 @@ public class App {
 		Player player = new Player(name, playerLocation);
 
 		// Spawn Objects
+		
+		// TEST
+		/*
+		int[] gobTestLoc = generateCoordinates(rows, columns);
+		Goblin gTest = new Goblin(gobTestLoc);
+		gTest.setSprite('G');
+		gameGrid.updateTile(gobTestLoc, gTest);
+		*/
+		// TEST
 
 		spawnTreasure(noOfTreasure, playerLocation, treasure, gameGrid);
 		spawnVillager(noOfVillagers, playerLocation, villager, gameGrid);
@@ -87,6 +96,7 @@ public class App {
 			else {
 
 				player.movePlayer(move);
+				//randomMove(gTest, gameGrid); // TEST
 
 				updateTreasureTile(treasure, gameGrid);
 				updateEnemyTile(enemy, gameGrid);
@@ -129,41 +139,78 @@ public class App {
 	}
 	
 	
-/*	public static void randomMove(Entity e, Grid g) {
+	public static void randomMove(Entity e, Grid g) {
 		
 		// Generate random position
-		int direction;
-		int step;
-		int[] newPos;
+		int xy_direction;
+		int pm;
+		int stepSize = 1;
+		int[] newPos = {0,0};
 		
-		while (true) {
-			direction = (int) Math.random(); // Picks x or y
-			step = (int) ((Math.random() * 2) - 1); // Random number between -1 and 1
+		int calcCount = 0;
+		
+		boolean isValid = false;
+		while (calcCount < 5 && !isValid) {
+			
+			xy_direction = (int) (Math.random()*2); // Picks x or y
+			pm = (int) (Math.random()*2); // Picks positive or negative
+			int step = ((int) Math.pow(-1, pm)) * stepSize;
 			newPos = e.getPosition();
-			newPos[direction] += step; // Updates new position with random x/y step
+			newPos[xy_direction] += step; // Updates new position with random x/y step
+			
+			if (newPos[0] < 0 || newPos[1] < 0) {
+				continue;
+			}
+			
+			/*
+			System.out.println("Goblin random step: " + step);
+			System.out.println("Goblin random xy_d: " + xy_direction);
+			System.out.println("oldPos: " + e.getPosition()[0] + "," + e.getPosition()[1]);
+			System.out.println("newPos: " + newPos[0] + "," + newPos[1]);
+			*/
 			
 			// Check position is valid
 			// Boundary Check
-			if (newPos[0] == 0 || newPos[0] == g.getRows()-1 || newPos[1] == 0 || newPos[1] == g.getColumns()-1) {
-				continue;
-				
+			if (newPos[0] <= 0 || newPos[0] >= g.getRows()-1 || newPos[1] <= 0 || newPos[1] >= g.getColumns()-1) {
+				System.out.println("Boundary Check");
+				isValid = false;
 			}
 			
 			// Collision Check with treasure
 			for (Treasure t : treasure) {
 				if (newPos == t.getPosition()) {
-					continue;
-					
-				} else {
+					isValid = false;
 					break;
 				}
 			}
+			//System.out.println("Treasure Collision Check");
 			
-			// Check collision with NPCs and other Entities
+			// Collision Check with villager
+			for (Villager v : villager) {
+				if (newPos == v.getPosition()) {
+					isValid = false;
+					break;
+				}
+			}
+			//System.out.println("Villager Collision Check");
+			
+			// Collision Check with enemy
+			for (Enemy en : enemy) {
+				if (newPos == en.getPosition()) {
+					isValid = false;
+					break;
+				}
+			}
+			//System.out.println("Enemy Collision Check");
+			
+			// Check if the position leads to any collisions
+			if (isValid) {
+				break;
+			}
+			
+			calcCount++;
 			
 		}
-		
-		
 		
 		// Move entity
 		e.setPrevPosition(e.getPosition());
@@ -171,8 +218,13 @@ public class App {
 		
 		// Update map position
 		g.updateTile(e.getPosition(), e);
+		
+		System.out.println("oldPos: " + e.getPosition()[0] + "," + e.getPosition()[1]);
+		System.out.println("newPos: " + newPos[0] + "," + newPos[1]);
+		System.out.println("Move G from: " + e.getPrevPosition()[0] + "," + e.getPrevPosition()[1] + " to: " + e.getPosition()[0] + "," + e.getPosition()[1]);
+		
 	}
-*/
+
 // Spawn Objects
 
 	public static void spawnEnemy(int noOfObjects, int[] playerLocation, ArrayList<Enemy> e, Grid grid) {
--- src/test/java/com/sprint/game_text/AppTest.java
+++ src/test/java/com/sprint/game_text/AppTest.java
@@ -12,6 +12,7 @@ public class AppTest {
 	  
 	Player p1 = new Player("Player1", pos1);
 	Treasure t1 = new Treasure(pos2);
+	Grid g = new Grid(20, 20);
 	
     @Test
     @DisplayName("Distance Calculations")
@@ -21,5 +22,16 @@ public class AppTest {
     	double[] distVec = {pos1[0]-pos2[0], pos1[1]-pos2[1]};
     	double dist = Math.sqrt( (distVec[0] * distVec[0]) + (distVec[1] * distVec[1]) );
     	assertEquals(dist, App.getDistance(p1, t1));
+    }
+    
+    @Test
+    @DisplayName("Random Movement")
+    public void randomMovement() {
+    	System.out.println("\n\n");
+;    	p1.setPosition(pos1);
+    	Player testPrevPos = new Player("test", p1.getPosition());
+    	
+    	App.randomMove(p1, g);
+    	assertTrue(2 >= App.getDistance(p1, testPrevPos) );
     }
 }
