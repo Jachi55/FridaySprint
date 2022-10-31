b8a56c78280f28f407458626433d0983dc08c18c
Jachi <acejachi@gmail.com>
2022-10-31 09:27:09 +0000

    weekend update

--- src/main/java/com/sprint/game_text/App.java
+++ src/main/java/com/sprint/game_text/App.java
@@ -8,7 +8,7 @@ public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner (System.in);
     char move;
-   // double distanceFromTreasure;
+    double distanceFromTreasure;
 
     
     System.out.println("Welcome to Gridlock!!");
